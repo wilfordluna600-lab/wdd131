24710bd652143a972e156a2d36b145a125e663df
wilfordluna600-lab <wlunaescobar@byupathway.edu>
2025-11-07 17:08:37 -0500

    changes

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,2 +1,3 @@
-const year = document.querySelector("#year");
+const year = document.querySelector("#currentyear");
 year.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear()}</span>`;
+document.getElementById("lastModified").innerHTML = document.lastModified;
