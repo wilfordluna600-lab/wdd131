49ee905c20a8cf95d67e5d9690a4c0548a2acdca
wilfordluna600-lab <wlunaescobar@byupathway.edu>
2025-11-15 18:52:38 -0500

    changes

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,12 +1,12 @@
-const input = document.querySelector('#favchap');
-const button = document.querySelector('button');
-const list = document.querySelector('#list'); // you need to fill in the blank to reference the HTML element that is a unordered list element.
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-list.append(li);
-button.addEventListener('click', function() {
-    if (input.value.trim() !== '') { ... }
-});
+// const input = document.querySelector('#favchap');
+// const button = document.querySelector('button');
+// const list = document.querySelector('#list'); // you need to fill in the blank to reference the HTML element that is a unordered list element.
+// const li = document.createElement('li');
+// const deleteButton = document.createElement('button');
+// li.textContent = input.value;
+// deleteButton.textContent = '❌';
+// li.append(deleteButton);
+// list.append(li);
+// button.addEventListener('click', function() {
+//     if (input.value.trim() !== '') { ... }
+// });
