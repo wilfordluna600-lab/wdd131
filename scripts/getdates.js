da0f1a36400ac163fcc6cb9f9d4728d09e15a050
wilfordluna600-lab <wlunaescobar@byupathway.edu>
2026-01-09 20:27:35 -0500

    week one changes

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,5 @@
-const year = document.querySelector("#currentyear");
-year.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear()}</span>`;
+document.getElementById("lastModified").innerHTML = document.lastModified;
 
-const currentYear = new Date().getFullYear();
+const year = document.querySelector("#currentyear");
+const today = new Date();
+year.innerHTML = today.getFullYear();
