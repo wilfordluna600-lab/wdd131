4329ff0e7b496b5843c67c2a11a48664788a6664
wilfordluna600-lab <wlunaescobar@byupathway.edu>
2026-01-16 21:11:23 -0500

    changes

---
+++ week02/scripts/bom.js
@@ -0,0 +1,5 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+const li = document.createElement('li');
+const deleteButton = document.createElement('button');
