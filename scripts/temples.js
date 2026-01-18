40e43e38ab0fbbfa78232543f239e4d8c20bafd3
wilfordluna600-lab <wlunaescobar@byupathway.edu>
2026-01-17 20:21:34 -0500

    changes week two activities

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,2 +1,4 @@
 const currentYear = new Date().getFullYear();
 document.getElementById("currentyear").textContent = currentYear;
+
+document.getElementById("lastModified").innerHTML = document.lastModified;
--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -3,3 +3,7 @@ const button = document.querySelector('button');
 const list = document.querySelector('#list');
 const li = document.createElement('li');
 const deleteButton = document.createElement('button');
+li.textContent = input.value;
+deleteButton.textContent = '❌';
+li.append(deleteButton);
+list.append(li);
