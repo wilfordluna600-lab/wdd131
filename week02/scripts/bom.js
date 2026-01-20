8382cce42ce960cde5b93e12ca3cdb9851e26431
wilfordluna600-lab <wlunaescobar@byupathway.edu>
2026-01-19 19:32:17 -0500

    continuing with the week two activities

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -7,3 +7,19 @@ li.textContent = input.value;
 deleteButton.textContent = '❌';
 li.append(deleteButton);
 list.append(li);
+/* I have duplicated the code that is in line
+and in line 9, I want to remember this, and if
+it is necesary I'm going to delete it, for the
+time being these lines stay here. */
+button.addEventListener('click', function() {
+    li.textContent = input.value;
+    deleteButton.textContent = '❌';
+    li.append(deleteButton);
+    list.append(li);
+});
+deleteButton.addEventListener('click', function () {
+    list.removeChild(li);
+    input.focus();
+});
+input.value = '';
+input.focus();
