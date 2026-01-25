cac5713b42d90405b115f72eea6db7129d4a9ac1
wilfordluna600-lab <wlunaescobar@byupathway.edu>
2026-01-24 19:01:07 -0500

    last changes week two

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,3 +2,11 @@ const currentYear = new Date().getFullYear();
 document.getElementById("currentyear").textContent = currentYear;
 
 document.getElementById("lastModified").innerHTML = document.lastModified;
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
