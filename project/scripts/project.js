c10d030d6ab7d22f08c0d32c900ae906dc4f3ccb
wilfordluna600-lab <wlunaescobar@byupathway.edu>
2026-02-19 19:37:04 -0500

    changes

--- project/scripts/project.js
+++ project/scripts/project.js
@@ -10,13 +10,13 @@ const firstPage = [
   subtitle: "Club Members",
   content: "Our club has 60 members, with a president along with nine other members who run this club. We accept people who are of legal age and want to participate.",
   image: "images/image-running-one.jpg",
-  imageName: "first-image-running",
+  imageName: "first image running",
   },
   {
   subtitle: "Weekly Practices",
   content: "We have weekly practices at Pisco Beach and Paracas Beach, which are located in the city of Pisco; all members are invited to come and practice with the group while enjoying the relaxing view of the sea.",
   image: "images/image-running-two.jpg",
-  imageName: "second-image-running",
+  imageName: "second image running",
   },
 ];
 
@@ -25,7 +25,7 @@ createContentCard(firstPage);
 
 function createContentCard(showContent) {
   document.querySelector(".first-content").innerHTML = "";
-  showContent.forEach(content => {
+  showContent.forEach(contents => {
     let card = document.createElement("section");
     let eachSubtitle = document.createElement("h2");
     let eachContent = document.createElement("p");
